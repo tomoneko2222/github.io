b655e6a0d24f012678e106dc7db5afa066c1c0cd
tomoneko2222 <166003396+tomoneko2222@users.noreply.github.com>
2024-05-29 09:57:45 +0900

    Update main.js

--- main.js
+++ main.js
@@ -4,15 +4,18 @@ document.getElementById('startButton').addEventListener('click', () => {
     const webhookUrls = document.getElementById('webhookUrls').value.split('\n');
     const interval = parseFloat(document.getElementById('interval').value) * 1000; // convert to milliseconds
     const message = document.getElementById('message').value;
+    const embedMessage = document.getElementById('embedMessage').checked;
 
     webhookUrls.forEach(webhookUrl => {
         const intervalId = setInterval(() => {
+            const requestBody = embedMessage ? { embeds: [{ description: message }] } : { content: message };
+
             fetch(webhookUrl.trim(), {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify({ content: message })
+                body: JSON.stringify(requestBody)
             })
             .then(response => console.log(`メッセージが正常に送信されました: ${response.status}`))
             .catch(error => console.log(`メッセージの送信に失敗しました: ${error}`));
